e35ad159b2679b270163b94fb8320946923c3235
Trong <167620149+Bui-Minh-Trong@users.noreply.github.com>
2025-04-30 20:27:49 +0700

    fix

--- src/Config/DatabaseConnection.java
+++ src/Config/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/facilities_management";
+    private static final String URL = "jdbc:mysql://localhost:3306/";
     private static final String USER = "root";
-    private static final String PASSWORD = "26052005Nan@";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
